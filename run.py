3b4c71ad9dfc50a511d3ebef9613bc8e85bc89d4
Cloudflipper <cldflpr@umich.edu>
2025-04-06 00:35:58 -0400

    minor change

--- run.py
+++ run.py
@@ -116,69 +116,74 @@ def train(env, log_dir, model_dir, lr, device, batch_size, feature_type=0):
     
     writer.close()
 
+    
 def test(env, path_to_model, saved_data_dir, simulation_seconds):
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    actor = sac.PolicyNetwork(env.observation_space.shape[0], env.action_space.shape[0]).to(device)
-    state_dict = torch.load(path_to_model, map_location=torch.device(device=device))
-    state_dict = {k.replace("module.", ""): v for k, v in state_dict.items()}
-    actor.load_state_dict(state_dict)
-    os.makedirs(saved_data_dir, exist_ok=True)
+    """
+    Test is to be modified.
+    """
+    pass
+#     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+#     actor = sac.PolicyNetwork(env.observation_space.shape[0], env.action_space.shape[0]).to(device)
+#     state_dict = torch.load(path_to_model, map_location=torch.device(device=device))
+#     state_dict = {k.replace("module.", ""): v for k, v in state_dict.items()}
+#     actor.load_state_dict(state_dict)
+#     os.makedirs(saved_data_dir, exist_ok=True)
 
-    _, obs = env.reset()[0]
-    done = False
-    extra_steps = 500
+#     _, obs = env.reset()[0]
+#     done = False
+#     extra_steps = 500
 
-    dt = env.dt
-    actions_list = []
-    tendon_length_list = []
-    cap_posi_list = []
-    reward_forward_list = []
-    reward_ctrl_list = []
-    waypt_list = []
-    x_pos_list = []
-    y_pos_list = []
-    iter = int(simulation_seconds/dt)
-    for i in range(iter):
-        action_scaled, _ = actor.predict(torch.from_numpy(obs).float())
-        # action_unscaled = action_scaled.detach() * 0.3 - 0.15
-        action_unscaled = action_scaled.detach() * 0.05
-        _, obs, _, done, _, info = env.step(action_unscaled.numpy())
+#     dt = env.dt
+#     actions_list = []
+#     tendon_length_list = []
+#     cap_posi_list = []
+#     reward_forward_list = []
+#     reward_ctrl_list = []
+#     waypt_list = []
+#     x_pos_list = []
+#     y_pos_list = []
+#     iter = int(simulation_seconds/dt)
+#     for i in range(iter):
+#         action_scaled, _ = actor.predict(torch.from_numpy(obs).float())
+#         # action_unscaled = action_scaled.detach() * 0.3 - 0.15
+#         action_unscaled = action_scaled.detach() * 0.05
+#         _, obs, _, done, _, info = env.step(action_unscaled.numpy())
 
 
 
-        actions_list.append(action_unscaled.numpy())
-        #the tendon lengths are the last 9 observations
-        # tendon_length_list.append(obs[-9:])
-        tendon_length_list.append(info["tendon_length"])
-        cap_posi_list.append(info["real_observation"][:18])
-        reward_forward_list.append(info["reward_forward"])
-        reward_ctrl_list.append(info["reward_ctrl"])
-        waypt_list.append(info["waypt"])
-        x_pos_list.append(info["x_position"])
-        y_pos_list.append(info["y_position"])
+#         actions_list.append(action_unscaled.numpy())
+#         #the tendon lengths are the last 9 observations
+#         # tendon_length_list.append(obs[-9:])
+#         tendon_length_list.append(info["tendon_length"])
+#         cap_posi_list.append(info["real_observation"][:18])
+#         reward_forward_list.append(info["reward_forward"])
+#         reward_ctrl_list.append(info["reward_ctrl"])
+#         waypt_list.append(info["waypt"])
+#         x_pos_list.append(info["x_position"])
+#         y_pos_list.append(info["y_position"])
 
-        if done:
-            extra_steps -= 1
+#         if done:
+#             extra_steps -= 1
 
-            if extra_steps < 0:
-                break
+#             if extra_steps < 0:
+#                 break
 
-    action_array = np.array(actions_list)
-    tendon_length_array = np.array(tendon_length_list)
-    cap_posi_array = np.array(cap_posi_list)
-    reward_forward_array = np.array(reward_forward_list)
-    reward_ctrl_array = np.array(reward_ctrl_list)
-    waypt_array = np.array(waypt_list)
-    x_pos_array = np.array(x_pos_list)
-    y_pos_array = np.array(y_pos_list)
-    np.save(os.path.join(saved_data_dir, "action_data.npy"),action_array)
-    np.save(os.path.join(saved_data_dir, "tendon_data.npy"),tendon_length_array)
-    np.save(os.path.join(saved_data_dir, "cap_posi_data.npy"),cap_posi_array)
-    np.save(os.path.join(saved_data_dir, "reward_forward_data.npy"),reward_forward_array)
-    np.save(os.path.join(saved_data_dir, "reward_ctrl_data.npy"),reward_ctrl_array)
-    np.save(os.path.join(saved_data_dir, "waypt_data.npy"),waypt_array)
-    np.save(os.path.join(saved_data_dir, "x_pos_data.npy"),x_pos_array)
-    np.save(os.path.join(saved_data_dir, "y_pos_data.npy"),y_pos_array)
+#     action_array = np.array(actions_list)
+#     tendon_length_array = np.array(tendon_length_list)
+#     cap_posi_array = np.array(cap_posi_list)
+#     reward_forward_array = np.array(reward_forward_list)
+#     reward_ctrl_array = np.array(reward_ctrl_list)
+#     waypt_array = np.array(waypt_list)
+#     x_pos_array = np.array(x_pos_list)
+#     y_pos_array = np.array(y_pos_list)
+#     np.save(os.path.join(saved_data_dir, "action_data.npy"),action_array)
+#     np.save(os.path.join(saved_data_dir, "tendon_data.npy"),tendon_length_array)
+#     np.save(os.path.join(saved_data_dir, "cap_posi_data.npy"),cap_posi_array)
+#     np.save(os.path.join(saved_data_dir, "reward_forward_data.npy"),reward_forward_array)
+#     np.save(os.path.join(saved_data_dir, "reward_ctrl_data.npy"),reward_ctrl_array)
+#     np.save(os.path.join(saved_data_dir, "waypt_data.npy"),waypt_array)
+#     np.save(os.path.join(saved_data_dir, "x_pos_data.npy"),x_pos_array)
+#     np.save(os.path.join(saved_data_dir, "y_pos_data.npy"),y_pos_array)
 
 
 # Training loop
