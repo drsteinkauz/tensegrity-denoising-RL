e6cc17f92a5f934832eb33575dc3fb513528bf82
Chi Zhang <zhc@umich.edu>
2025-05-23 21:46:40 -0400

    assymetric -> symetric (remove state, use observation only)

--- ipe_sac.py
+++ ipe_sac.py
@@ -280,12 +280,13 @@ class SACAgent:
         with torch.no_grad():
             next_feature_obs_batch = torch.cat([next_observation_batch, latent_batch], dim=-1)
             sampled_action_next, action_log_prob_next, _ = self.actor.sample(next_feature_obs_batch)
-            next_feature_state_batch = torch.cat([next_state_batch, latent_batch], dim=-1)
-            q1_target_next_pi, q2_target_next_pi = self.target_critic(next_feature_state_batch, sampled_action_next)
+            # next_feature_state_batch = torch.cat([next_state_batch, latent_batch], dim=-1)
+            # q1_target_next_pi, q2_target_next_pi = self.target_critic(next_feature_state_batch, sampled_action_next)
+            q1_target_next_pi, q2_target_next_pi = self.target_critic(next_feature_obs_batch, sampled_action_next)
             q_target_next_pi = torch.min(q1_target_next_pi, q2_target_next_pi)
             next_q_value = reward_batch.view(-1, 1) + self.gamma * (1 - done_batch.view(-1, 1)) * (q_target_next_pi - self.alpha * action_log_prob_next)
-            feature_state_batch = torch.cat([state_batch, latent_batch], dim=-1)
-        q1_value, q2_value = self.critic(feature_state_batch, action_batch)
+            # feature_state_batch = torch.cat([state_batch, latent_batch], dim=-1)
+        q1_value, q2_value = self.critic(feature_obs_batch.detach(), action_batch)
         critic1_loss = F.mse_loss(q1_value, next_q_value)
         critic2_loss = F.mse_loss(q2_value, next_q_value)
         critic_loss = (critic1_loss + critic2_loss) / 2
@@ -295,7 +296,7 @@ class SACAgent:
         self.critic_optimizer.step()
         
         # Actor update
-        q1_pi, q2_pi = self.critic(feature_state_batch, sampled_action)
+        q1_pi, q2_pi = self.critic(feature_obs_batch, sampled_action)
         q_value_pi = torch.min(q1_pi, q2_pi)
         actor_loss = (self.alpha * action_log_prob - q_value_pi).mean()
 
